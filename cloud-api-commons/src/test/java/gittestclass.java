2f658b42c427455d468082cf42aa47589f506766
lichengzhi <1529775939@qq.com>
2022-09-07 14:47:21 +0800

    cloud2020 third commit

--- cloud-api-commons/src/test/java/gittestclass.java
+++ cloud-api-commons/src/test/java/gittestclass.java
@@ -7,4 +7,5 @@ import lombok.extern.slf4j.Slf4j;
  */
 public class gittestclass {
      //1111111
+    //222
 }
---
+++ cloud-api-commons/src/test/java/test2.java
@@ -0,0 +1,6 @@
+/**
+ * @author Chengzhi Li
+ * @date 2022/9/7 14:44 星期三
+ */
+public class test2 {
+}
